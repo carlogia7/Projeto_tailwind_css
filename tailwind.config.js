1ce53aa12bb96742bbe9077e765ac5cd393c4a1a
Carlo <101676913+carloo7@users.noreply.github.com>
2024-03-18 10:55:28 -0300

    Add files via upload
    Arquivos para o projeto

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        monda: ['Monda  ']
+      },
+      colors: {
+        'azul-tailwind': '#38bdf8',
+      }
+    },
+  },
+  plugins: [],
+}
+
