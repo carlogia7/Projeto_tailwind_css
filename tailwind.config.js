0d1d43829d981207f9d693f60c41c8ad61c587a2
Carlo <carloggiacometti77@gmail.com>
2024-05-08 17:35:41 -0300

    Fazendo responsividade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'custom': '1400px',
+    },
     extend: {
       fontFamily: {
         monda: ['Monda'],
